459a4145b672ba68cf3d01df1a834d33b628f982
Sheldon Barry <sheldonbarry@iafrica.com>
2017-09-24 23:03:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# csv2sqlite
+
+A simple Python module to convert CSV to a SQLite database.
+
+There is experimental support for guessing the data type between INTEGER and TEXT. 
+
+This utility is work in progress and no guarantees are made as to its effectiveness.
---
+++ csv2sqlite/csv2sqlite.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# a simple module to convert CSV file to SQLite database
+# all fields treated as text
+
+# Copyright SR Barry 2017
+
+import csv
+import sqlite3
+
+# extract csv file and put it in a list (rows) of lists (fields)
+def csv_parse(filename, Delimiter, Quotechar='"'):
+    with open(filename, 'rU') as a:
+        data = csv.reader(a, delimiter=Delimiter, quotechar=Quotechar)
+        parsed_list = []
+        # extract data and return a list of lists
+        for row in data:        
+            parsed_list.append(row)
+    return parsed_list
+
+# function to create table
+def do_table(db_name, tb_name, tb_data, data_dict=None):
+
+    # set table header field data
+    if data_dict == None:    
+        field_data = ''
+        for field in tb_data[0]:
+            field_data = field_data + '%s TEXT, ' % field
+    else:
+        field_data = ''
+        for field in data_dict:
+            field_data = field_data + field[0] + ' ' + field[1] + ', ' 
+    # trim trailing comma and space
+    field_data = field_data[:-2]
+
+    # create a list to hold data not added to database table
+    errors = []
+
+    with sqlite3.connect(db_name) as con:
+        
+        # need this to work with special characters
+        con.text_factory = str
+        # set cursor
+        cur = con.cursor()
+        
+        # check to see if table exists and make a fresh table.
+        cur.execute("DROP TABLE IF EXISTS %s" % tb_name)
+        cur.execute("CREATE TABLE %s (%s)" % (tb_name, field_data))
+        con.commit()
+
+        # insert data into table
+        for row in tb_data[1:]:
+            try:
+                # create string of row values to insert into database
+                values = ''
+                for field in row:
+                    values = values + "'" + field.decode('utf-8') + "'" + ','
+                values = values[:-1]
+                values = '(' + values + ')'
+            
+                # insert data and update
+                cur.execute("INSERT INTO %s VALUES %s" % (tb_name, values))                
+                con.commit()
+            except:
+                errors.append(tb_data[row])
+                
+        # write errors to log
+        with open('error_log.txt', 'wr') as a:
+            a.write("The following records were not added because they had errors:\n")
+            for item in errors:
+                a.write(str(item)+'\n')
+
+def create_data_dict(filename, Delimiter, Quotechar, key=None):
+    """Create a data dictionary in list format of field names and data types"""
+    with open(filename, 'rU') as a:
+        data = csv.reader(a, delimiter=Delimiter, quotechar=Quotechar)
+        # get header info
+        field_names = [field for field in next(data)] 
+        # determine data type and store in data_types list variable       
+        data_types = ['' for item in field_names]
+        for row in data:
+            for row_index in range(len(row)):
+                try:
+                    if int(row[row_index]) % 1 == 0 and data_types[row_index] != 'TEXT':
+                        data_types[row_index] = 'INTEGER'
+                except:
+                    data_types[row_index] = 'TEXT' 
+        if key in field_names:
+            data_types[field_names.index(key)] += ' PRIMARY KEY'                     
+    return zip(field_names, data_types)
+
